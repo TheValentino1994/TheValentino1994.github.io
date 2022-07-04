023aba1aca020d80921e194d0715311b13aa1593
MaksymTeplyi <maksym.teplyi@inmost.pro>
2022-07-05 00:05:17 +0300

    Set media files from aws

--- src/constants/_images.ts
+++ src/constants/_images.ts
@@ -12,40 +12,6 @@ import { HomeIntroWave } from "../assets/images/illustrations/HomeIntroWave";
 /* ------------------------------- Backgrounds ------------------------------ */
 import FooterBg from "../assets/images/backgrounds/footerBg.svg";
 
-/* ----------------------------- Projects photos ---------------------------- */
-import LexerPreview from "../assets/images/projectsPhotos/Lexer/preview.png";
-import LexerDesignSystem from "../assets/images/projectsPhotos/Lexer/designSystem.png";
-import LexerSlide1_1 from "../assets/images/projectsPhotos/Lexer/slides1/slide1.png";
-import LexerSlide1_2 from "../assets/images/projectsPhotos/Lexer/slides1/slide2.png";
-import LexerSlide1_3 from "../assets/images/projectsPhotos/Lexer/slides1/slide3.png";
-import LexerSlide1_4 from "../assets/images/projectsPhotos/Lexer/slides1/slide4.png";
-import LexerSlide1_5 from "../assets/images/projectsPhotos/Lexer/slides1/slide5.png";
-import LexerSlide1_6 from "../assets/images/projectsPhotos/Lexer/slides1/slide6.png";
-import LexerSlide2_1_1 from "../assets/images/projectsPhotos/Lexer/slides2/slide1_1.png";
-import LexerSlide2_1_2 from "../assets/images/projectsPhotos/Lexer/slides2/slide1_2.png";
-import LexerSlide2_1_3 from "../assets/images/projectsPhotos/Lexer/slides2/slide1_3.png";
-import LexerSlide2_1_4 from "../assets/images/projectsPhotos/Lexer/slides2/slide1_4.png";
-import LexerSlide2_2_1 from "../assets/images/projectsPhotos/Lexer/slides2/slide2_1.png";
-import LexerSlide2_2_2 from "../assets/images/projectsPhotos/Lexer/slides2/slide2_2.png";
-import LexerSlide2_2_3 from "../assets/images/projectsPhotos/Lexer/slides2/slide2_3.png";
-import LexerSlide2_2_4 from "../assets/images/projectsPhotos/Lexer/slides2/slide2_4.png";
-import LexerSlide2_3_1 from "../assets/images/projectsPhotos/Lexer/slides2/slide3_1.png";
-import LexerSlide2_3_2 from "../assets/images/projectsPhotos/Lexer/slides2/slide3_2.png";
-import LexerSlide2_3_3 from "../assets/images/projectsPhotos/Lexer/slides2/slide3_3.png";
-import LexerSlide2_3_4 from "../assets/images/projectsPhotos/Lexer/slides2/slide3_4.png";
-
-import WingTipzPreview from "../assets/images/projectsPhotos/WingTipz/preview.png";
-import WingTipzIntro from "../assets/images/projectsPhotos/WingTipz/intro.png";
-import WingTipzResult from "../assets/images/projectsPhotos/WingTipz/result.png";
-
-import WildMindsrPreview from "../assets/images/projectsPhotos/WildMinds/preview.png";
-import WildMindsrResult from "../assets/images/projectsPhotos/WildMinds/result.png";
-import WildMindsrBg1 from "../assets/images/projectsPhotos/WildMinds/bg1.png";
-import WildMindsrBg2 from "../assets/images/projectsPhotos/WildMinds/bg2.png";
-import WildMindsrBg3 from "../assets/images/projectsPhotos/WildMinds/bg3.png";
-import WildMindsrBg4 from "../assets/images/projectsPhotos/WildMinds/bg4.png";
-import WildMindsrBg5 from "../assets/images/projectsPhotos/WildMinds/bg5.png";
-
 export const IMAGES = {
   Illustrations: {
     Planet1,
@@ -65,35 +31,58 @@ export const IMAGES = {
 
   projectsPhotos: {
     wildMinds: {
-      preview: WildMindsrPreview,
-      result: WildMindsrResult,
-      bg1: WildMindsrBg1,
-      bg2: WildMindsrBg2,
-      bg3: WildMindsrBg3,
-      bg4: WildMindsrBg4,
-      bg5: WildMindsrBg5,
+      preview:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/preview.png",
+      result:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/result.png",
+      bg1: "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/bg1.png",
+      bg2: "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/bg2.png",
+      bg3: "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/bg3.png",
+      bg4: "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/bg4.png",
+      bg5: "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WildMinds/bg5.png",
     },
     wingTipz: {
-      preview: WingTipzPreview,
-      intro: WingTipzIntro,
-      result: WingTipzResult,
+      preview:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WingTipz/preview.png",
+      intro:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WingTipz/intro.png",
+      result:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/WingTipz/result.png",
     },
     lexer: {
-      preview: LexerPreview,
-      designSystem: LexerDesignSystem,
+      preview:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/preview.png",
+      designSystem:
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/designSystem.png",
       slides1: [
-        LexerSlide1_1,
-        LexerSlide1_2,
-        LexerSlide1_3,
-        LexerSlide1_4,
-        LexerSlide1_5,
-        LexerSlide1_6,
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides1/slide1.png",
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides1/slide2.png",
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides1/slide3.png",
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides1/slide4.png",
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides1/slide5.png",
+        "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides1/slide6.png",
       ],
       slides2: [
-        [LexerSlide2_1_1, LexerSlide2_2_1, LexerSlide2_3_1],
-        [LexerSlide2_1_2, LexerSlide2_2_2, LexerSlide2_3_2],
-        [LexerSlide2_1_3, LexerSlide2_2_3, LexerSlide2_3_3],
-        [LexerSlide2_1_4, LexerSlide2_2_4, LexerSlide2_3_4],
+        [
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide1_1.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide2_1.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide3_1.png",
+        ],
+        [
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide1_2.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide2_2.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide3_2.png",
+        ],
+        [
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide1_3.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide2_3.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide3_3.png",
+        ],
+        [
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide1_4.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide2_4.png",
+          "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Images/Lexer/slides2/slide3_4.png",
+        ],
       ],
     },
   },
--- src/constants/_videos.ts
+++ src/constants/_videos.ts
@@ -1,20 +1,37 @@
 export const VIDEOS = {
   lexer: {
-    intro: require("../assets/Lexer1.mov"),
-    challanges1: require("../assets/Lexer2.mov"),
+    intro:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Lexer1.mov",
+
+    challanges1:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/Lexer2.mov",
   },
 
   wingTipz: {
-    uiProcess1: require("../assets/WingTipz1.mov"),
-    uiProcess2: require("../assets/WingTipz2.mov"),
-    challenges1: require("../assets/WingTipz3.mov"),
+    uiProcess1:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WingTipz1.mov",
+
+    uiProcess2:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WingTipz2.mov",
+
+    challenges1:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WingTipz3.mov",
   },
 
   wildMinds: {
-    intro: require("../assets/WildMinds1.mov"),
-    challenges1: require("../assets/WildMinds2.MP4"),
-    challenges2: require("../assets/WildMinds3.mov"),
-    uiProcess1: require("../assets/WildMinds4.MP4"),
-    uiProcess2: require("../assets/WildMinds5.MP4"),
+    intro:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WildMinds1.mov",
+
+    challenges1:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WildMinds2.MP4",
+
+    challenges2:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WildMinds3.mov",
+
+    uiProcess1:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WildMinds4.MP4",
+
+    uiProcess2:
+      "https://portfoliovalentino.s3.eu-central-1.amazonaws.com/WildMinds5.MP4",
   },
 };
