8d74193962ea5837ee7ca519edb749fd3df65618
Valentino <valentino@MacBook-Pro-Valentino.local>
2026-05-03 12:16:40 +0300

    initial commit

--- src/blockPages/portfolio/wildMinds/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Intro } from "./Intro";
-export { default as Challenges } from "./Challenges";
-export { default as UiProcess } from "./UIProcess";
--- src/constants/_routes.ts
+++
@@ -1,7 +0,0 @@
-export enum ROUTES {
-  HOME = "/",
-  PORTFOLIO = "/portfolio",
-  PORTFOLIO_WILD_MINDS = "/portfolio/wildMinds",
-  PORTFOLIO_WING_TIPZ = "/portfolio/wingTipz",
-  PORTFOLIO_LEXER = "/portfolio/lexer",
-}
